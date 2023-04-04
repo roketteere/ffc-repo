1cdd79e047342dc5eec4a5eea5ffd46b7dfbe3cf
Joel Perez <jxp489@gmail.com>
2023-04-03 22:10:10 -0700

    added style.css notes for everything and added TODO's to my JavaScript.js file. pseudo code to be exact.

--- JavaScript/naruto-quiz-app/assets/js/script.js
+++ JavaScript/naruto-quiz-app/assets/js/script.js
@@ -1,2 +1,6 @@
 console.log('Logging');
 // TODO Create github repo with branch names for each username and upload project
+
+//TODO create a string variable that holds the #score and pre-formatting
+
+// TODO create a string variable to display the userInitials data
