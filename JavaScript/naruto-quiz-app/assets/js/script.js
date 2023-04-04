e766c08445e885854d16caeca43f18ab56b0486e
Joel Perez <jxp489@gmail.com>
2023-04-03 23:07:02 -0700

    forgot to hide the quiz container, timer container , and info container on load via CSS so I changed it to reflect display none on all of them. Will use JS to display them flex again

--- JavaScript/naruto-quiz-app/assets/js/script.js
+++ JavaScript/naruto-quiz-app/assets/js/script.js
@@ -1,4 +1,6 @@
+// Test and make sure our script loaded at page load/reload
 console.log('Logging');
+
 // TODO Create github repo with branch names for each username and upload project
 
 //TODO create a string variable that holds the #score and pre-formatting
