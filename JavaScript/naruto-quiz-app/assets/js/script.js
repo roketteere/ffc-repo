b887e1f1ee2b68c594039b1e38e5abb278eaf319
Joel Perez <jxp489@gmail.com>
2023-04-05 00:31:47 -0700

    final project push

--- JavaScript/naruto-quiz-app/assets/js/script.js
+++ JavaScript/naruto-quiz-app/assets/js/script.js
@@ -2,21 +2,39 @@
 console.log('Logging');
 
 // declare global variables
-var timerTime = 60;//for 60 secs per 1000 milliseconds
-// keep track of our current question index of questions asked so we dont duplicate
-// boolean variable to see if we start the quiz
-var isStarted = false;
+var timerTime = 300;
 var currentQuestionIndex = 0;
+var correct = null;
+var clicked = null;
+var amount = 10;
 // score variable to keep track of score
+var highScore = 0;
 var score = 0;
 // Create variable to hold user input initials
-var inputedInitials ='';
+var inputedInitials =[];
+var scored = [];
 // create an array to hold all the questions asked
 var questionsAsked = [];
+// loaded questions and answers
+var loadedQuiz = [];
+
+var quizQuestions = fetch('assets/json/questions.json').then((response) => response.json()).then((data) => {
+    var newData  = JSON.stringify(data);
+    localStorage.setItem('quiz-data', newData);
+    loadedQuiz = JSON.parse(localStorage.getItem('quiz-data'));
+    if('quiz-data' )
+    console.log('newData', newData);
+    if('user-data'){
+        scoreCard.textContent = localStorage('user-score')
+    }
+   
+  
+});
 
 // get our start button
 var startButton = document.querySelector('#start');
 var stopButton = document.querySelector('#stop');
+
 // get hidden container reference variables
 var quizContainer = document.querySelector('#quiz');
 var timerContainer = document.querySelector('#timer-container');
@@ -24,20 +42,45 @@ var infoContainer = document.querySelector('#info');
 
 // Get all other elements needed as reference variables (variables we created and named whatever we wanted so that we can access them in the html document)
 var pageTitle = document.querySelector('#title');
-var question = document.querySelector('#question');
-var answerOne = document.querySelector('a1');
-var answerTwo = document.querySelector('a2');
-var answerThree = document.querySelector('a3');
-var answerFour = document.querySelector('a4');
+var questionBox = document.querySelector('#question');
+var answerOne = document.querySelector('#a1');
+var answerTwo = document.querySelector('#a2');
+var answerThree = document.querySelector('#a3');
+var answerFour = document.querySelector('#a4');
+
+// Create array to hold answers
+var question =[answerOne, answerTwo, answerThree, answerFour];
 var timer = document.querySelector('#timer');
 var scoreCard = document.querySelector('#score');
 var userInitials = document.querySelector('#initials');
 
-//TODO create a string variable that holds the #score and pre-formatting
+// string variable that holds the #score and pre-formatting
 var scoreText = `Final Score: ${score}`;
 
-// TODO create a string variable to display the userInitials data
+//  Create a string variable to display the userInitials data
 var inputedInitialsText = `User Initials: ${inputedInitials}`;
+
+// function display questions
+function loadStoredQuiz(loadedQuiz){
+    loadedQuiz = JSON.parse(localStorage.getItem('quiz-data'));
+    rounds = loadedQuiz.quiz.length;
+    
+}
+
+// function to display answers
+function insertQuizItems(loadedQuiz, currentQuestionIndex){
+    questionBox.textContent = JSON.stringify(loadedQuiz.quiz[currentQuestionIndex].question);
+    answerOne.textContent = JSON.stringify(loadedQuiz.quiz[currentQuestionIndex].answers[0]);
+    answerTwo.textContent = JSON.stringify(loadedQuiz.quiz[currentQuestionIndex].answers[1]);
+    answerThree.textContent = JSON.stringify(loadedQuiz.quiz[currentQuestionIndex].answers[2]);
+    answerFour.textContent = JSON.stringify(loadedQuiz.quiz[currentQuestionIndex].answers[3]);
+    correct = JSON.stringify(loadedQuiz.quiz[currentQuestionIndex].correctAnswer);
+    questionsAsked.push(questionBox.textContent)
+    return parseInt(correct) ;
+
+
+    
+}
 
 // Create a startQuiz() function
 function displayQuiz(){
@@ -46,13 +89,13 @@ function displayQuiz(){
     timerContainer.setAttribute('style', 'display:flex;');
     infoContainer.setAttribute('style', 'display:flex;');
     console.log('Quiz Displayed!');
-    isStarted = true;
     
 }
 
 // function that starts the timer
 function timerStart(){
     displayQuiz();
+    correct = insertQuizItems(loadedQuiz,currentQuestionIndex);
     var timeInterval = setInterval (() => {
         if(timerTime > 0){
             timerTime--;
@@ -60,12 +103,22 @@ function timerStart(){
             console.log(`Time remaining: ${timerTime}`);
         }
        
-        else if(timerTime === 0){
+        else if(timerTime === 0 || questionsAsked.length ===  10){
             clearInterval(timeInterval);
-            timerTime = 60;
-            if(questionsAsked.length === 50){
-                alert("Quiz Completed Calculating Results!")
+            timerTime = 300;
+            highScore = (score + timerTime);
+            if(questionsAsked.length === 10){
+                console.log("Quiz Completed Calculating Results!");
             }
+            inputedInitials.push(prompt('Please enter Initials'));
+            for(i=0;i<inputedInitials.length;i++){
+                userInitials.textContent = inputedInitials[i];
+            }
+            scoreCard.textContent = highScore;
+            localStorage.setItem('user-score', highScore);
+            localStorage.setItem('user-initials',userInitials[-1]);
+            
+
         }
         
     }, 1000);
@@ -73,10 +126,81 @@ function timerStart(){
     
 }
 // add event listener to start button
+answerOne.addEventListener('click', function (event){
+    clicked = 0;
+    if(correct == clicked){
+        if(questionsAsked.length === 10){
+            timerTime = 0;
+            return;
+        }
+        currentQuestionIndex++;
+        score++;
+        correct = insertQuizItems(loadedQuiz,currentQuestionIndex);
+        console.log('Correct!');
+    }else console.log('Incorrect');
+    
+
+});
+// add event listener to start button
+answerTwo.addEventListener('click', function (event){
+    clicked = 1;
+    if(correct == clicked ){
+        
+        if(questionsAsked.length === 10){
+            timerTime = 0;
+            return;
+        }
+        currentQuestionIndex++;
+        score++;
+        correct = insertQuizItems(loadedQuiz,currentQuestionIndex);
+        console.log('Correct!');
+    }else {
+        score--;
+        console.log('Incorrect');
+    }
+    
+});
+// add event listener to start button
+answerThree.addEventListener('click', function (event){
+    clicked = 2;
+    if(correct == clicked){
+        if(questionsAsked.length === 10){
+            timerTime = 0;
+            return;
+        }
+        currentQuestionIndex++;
+        score++;
+        correct = insertQuizItems(loadedQuiz,currentQuestionIndex);
+        console.log('Correct!');
+    }else {
+        score--;
+        console.log('Incorrect');
+    }
+});
+// add event listener to start button
+answerFour.addEventListener('click', function (event){
+    clicked = 3;
+    if(correct == clicked){
+        
+        if(questionsAsked.length === 10){
+            timerTime = 0;
+            return;
+        }
+        currentQuestionIndex++;
+        score++;
+        correct = insertQuizItems(loadedQuiz,currentQuestionIndex);
+        console.log('Correct!');
+    }else {
+        score--;
+        console.log('Incorrect');
+    }
+    
+    
+});
+// add event listener to start button
 startButton.addEventListener('click', function (event){
-    console.log(event.target);
-    console.log('Start Button Clicked');
     timerStart();
+    console.log('Started');
     
 });
 
